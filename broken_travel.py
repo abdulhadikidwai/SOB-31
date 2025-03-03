74768e8951d822040be4112808a5636c2f26c2b4
abdul hadi <a00000h00000k@gmail.com>
2025-03-03 10:23:39 +0400

    SYNTAX FIXED

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,13 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year == int.input("Greetings! What is your year of origin? '))
+year = int(input("Greetings! What is your year of origin? ")) #chnaged == to = and . to ( and ' to "
 
-if year <= 1900
-    print ('Woah, that's the past!')
-elif year > 1900 && year < 2020:
+if year < 1900: #removed = and added colon
+    print ("Woah, that's the past!") # Replaced ' to "
+elif year >= 1900 and year <= 2020: # Replaced && with and, fixed condition
     print ("That's totally the present!")
-elif:
+else: #replaced elif with else
     print ("Far out, that's the future!!")
+
+    # By- Abdul Hadi Kidwai (M00990950)
