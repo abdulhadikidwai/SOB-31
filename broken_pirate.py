30a4870646e9d7edfb9c22ebf90e5dff2a7bde96
abdul hadi <a00000h00000k@gmail.com>
2025-03-03 10:15:12 +0400

    SYNTAX FIXED

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,7 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?") # Fixed missing quotation mark
+if greeting in ["Arrr!"]: # changed ) bracket to ]
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: # Replaced elif to else
+	print("Greetings, hater of pirates!")
+
+# By- Abdul Hadi Kidwai (M00990950)
